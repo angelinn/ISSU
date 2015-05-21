1b76d0ddf6a8b4efe2717aa3c6dc3fc338ec192f
Angelin Nedelchev <betrakiss@hotmail.com>
2015-05-21 19:52:57 +0300

    Removing old files.

---
+++ ISSU.OneDrive/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneDriveRestAPI;
+
+namespace ISSU.OneDrive
+{
+    public class Class1
+    {
+    }
+}
